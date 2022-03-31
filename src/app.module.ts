aa8cf5ab52e337007a1ed7d226a3bc1510f82242
Daniel Riccio <daniel@bikeexchange.co>
2022-03-31 18:30:03 -0500

    I dont even know what this is

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { Task } from './entities/task.entity';
@@ -6,7 +7,7 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([Task])],
+  imports: [ConfigModule, AuthModule, TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
   providers: [TasksService],
 })
